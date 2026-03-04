9650e937f36b1e786b55eecd7aad4c114cd3d179
Martin Kedmenec <martin.kedmenec@outlook.com>
2026-03-04 09:32:04 +0100

    Switch to Vite + React Compiler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      "@": resolve(import.meta.dirname, "src"),
     },
   },
 });
