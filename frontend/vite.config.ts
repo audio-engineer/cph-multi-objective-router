a81337a96242e3dd50a5f545ecfafea058d8b1d4
Martin Kedmenec <martin.kedmenec@outlook.com>
2026-03-16 08:58:23 +0100

    Update all packages. Update to Vite 8

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { resolve } from "node:path";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
     alias: {
       "@": resolve(import.meta.dirname, "src"),
